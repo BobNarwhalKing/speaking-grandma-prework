aa63d998eb5e29d5fb743fc91d3d8db415aa9a72
Kurtis Leach <danceflurry4366@gmail.com>
2019-05-08 22:42:30 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -1,5 +1,10 @@
 # Write a speak_to_grandma method.
-
+def speak_to_grandma (phrase)
+  if "#{phrase}" == uppercase
+    return "NO, NOT SINCE 1983!"
+  elsif "HUH?! SPEAK UP, SONNY!"
+  elsif "#{phrase}" == 'I LOVE YOU GRANDMA'
+    return 'I LOVE YOU TOO PUMPKIN!'
 # Whatever you say to grandma, she should respond with
 # HUH?! SPEAK UP, SONNY!
 # unless you shout it (type in all capitals).
